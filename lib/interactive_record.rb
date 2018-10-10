d523e5ea1bec87878b6852fc2344724c1967d501
Robert Snyder <r.snyder49@gmail.com>
2018-10-10 20:20:03 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -21,8 +21,8 @@ class InteractiveRecord
     column_names.compact 
   end 
   
-  self.column_names.each do |col_name| 
-    attr_accessor col_name.to_sym 
+  self.column_names.each do |col| 
+    attr_accessor col.to_sym 
   end
   
   def initialize(options = {})
